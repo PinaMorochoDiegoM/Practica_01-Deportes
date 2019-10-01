c5b1aa31e920692d026fbf385d713d7c3d7bda67
Cristian <Cristian@DESKTOP-N33UUOM>
2019-10-01 15:54:52 -0500

    Paso Final Progarama Terminado, Comentado y Funcionando

---
+++ src/Secundaria/Torneo.java
@@ -0,0 +1,49 @@
+package Secundaria;
+/**
+* Esta clase Historial crea los objetos, ademas de los metodos de acceso de manera publica 
+ * 
+ * @author Piña Diego 
+ * @version 01/10/2019
+ * @see <a href = "https://github.com/PinaMorochoDiegoM/Pratica01PinaDiego/tree/master/src/ec/edu/ups" > GitHub.com – Practica 01 Deportes </a>
+ * 
+ */
+public class Torneo {
+    private String tipo;
+    private String premio;
+    private boolean propaganda;
+    
+    public void jugarClasificatoria(){
+        //Codigo no requerido para esta practica
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPremio() {
+        return premio;
+    }
+
+    public void setPremio(String premio) {
+        this.premio = premio;
+    }
+
+    public boolean getPropaganda() {
+        return propaganda;
+    }
+
+    public void setPropaganda(boolean propaganda) {
+        this.propaganda = propaganda;
+    }
+
+    @Override
+    public String toString() {
+        return "Torneo{" + "tipo=" + tipo + ", premio=" + premio + ", propaganda=" + propaganda + '}';
+    }
+    
+    
+}
